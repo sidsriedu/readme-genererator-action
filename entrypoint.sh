8ce7179590a029be2f05e969dd15a7fae1849616
sidsriedu <79398876+sidsriedu@users.noreply.github.com>
2024-02-09 08:49:19 +0000

    echo added

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,8 @@ git config --global user.name "${GITHUB_ACTOR}"
 git config --global user.email "${INPUT_EMAIL}"
 git config --global --add safe.directory /github/workspace
 
+echo $(java -version 2>&1 | head -n 1)
+
 javac /usr/bin/TextToMDConvertor.java
 java TextToMDConvertor
 
