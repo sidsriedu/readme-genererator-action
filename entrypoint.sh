8ea3f7a4b67f63a9f20d6960913eff4a8ddf6273
sidsriedu <79398876+sidsriedu@users.noreply.github.com>
2024-02-09 09:36:59 +0000

    echo added

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,15 +6,14 @@ git config --global user.name "${GITHUB_ACTOR}"
 git config --global user.email "${INPUT_EMAIL}"
 git config --global --add safe.directory /github/workspace
 
-echo $(java -version 2>&1 | head -n 1)
-ls
-pwd
-
-cd /usr/bin
-ls
-pwd
 javac /usr/bin/TextToMDConvertor.java
 java /usr/bin/TextToMDConvertor
+
+echo "============================================"
+cd /usr/bin
+pwd
+ls
+echo "============================================"
 
 git add -A && git commit -m "Update readme"
 git push --set-upstream main origin
