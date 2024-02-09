b72dafc51e8ebacc3af1ad7dfd5212a3f7306d0c
sidsriedu <79398876+sidsriedu@users.noreply.github.com>
2024-02-09 09:56:10 +0000

    push modified

--- Dockerfile
+++ Dockerfile
@@ -6,8 +6,6 @@ RUN apt-get update && apt-get install -y \
     default-jdk \
     git
 
-COPY TextToMDConvertor.java /usr/bin/TextToMDConvertor.java
-
 COPY entrypoint.sh /entrypoint.sh
 
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -6,20 +6,11 @@ git config --global user.name "${GITHUB_ACTOR}"
 git config --global user.email "${INPUT_EMAIL}"
 git config --global --add safe.directory /github/workspace
 
-echo "============================================"
-pwd
-ls
-echo "============================================"
 
 javac TextToMDConvertor.java
 java TextToMDConvertor
 
-echo "============================================"
-pwd
-ls
-echo "============================================"
-
 git add -A && git commit -m "Update readme"
-git push --set-upstream main origin
+git push --set-upstream origin main
 
 echo "============================================"
